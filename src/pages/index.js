f0e8d5f1feb1c29153ed83d461b4f2272a9203ae
Baran Cezayirli <barancezayirlioglu@gmail.com>
2019-12-30 16:48:19 +0200

    init, add tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react';
+import '../main.css';
 
-export default () => <div>Hello world!</div>
+const Home = () => (
+  <main className="antialiased text-neutral-900 bg-neutral-100 min-h-screen p-5">
+    <div className="container mx-auto shadow bg-white">
+      Hello World
+    </div>
+  </main>
+);
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    colors: {
+      primary: colors.teal,
+      secondary: colors.red,
+      neutral: colors.gray,
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
